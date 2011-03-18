49fc52e263494f04391885fb4ca7db9f9cfaebad
ncp220891@gmail.com <ncp220891@gmail.com@fd7f2791-73e2-7d35-b9a1-f80d54f97a9f>
2011-03-18 10:14:07 +0000

    Voc thu ti thoi, Khong thay doi gi nhieu lam

--- trunk/MusicStore/Controllers/HomeController.cs
+++ trunk/MusicStore/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MusicStore.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Nhạc người lớn";
 
             return View();
         }
